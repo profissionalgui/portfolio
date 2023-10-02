d98e80017bd0b1b1ae5c69a37ee8001a99132f1e
Guilherme Henrique <120285403+profissionalgui@users.noreply.github.com>
2023-10-02 17:37:05 -0300

    Add files via upload

---
+++ src/components/InformationContainer.jsx
@@ -0,0 +1,34 @@
+import "../styles/components/informationcontainer.sass";
+
+import { AiFillPhone, AiOutlineMail, AiFillEnvironment } from "react-icons/ai";
+
+const InformationContainer = () => {
+  return (
+    <section id="information">
+      <div className="info-card">
+        <AiFillPhone id="phone-icon" />
+        <div>
+          <h3>Telefone</h3>
+          <p>(83)99185-1957</p>
+        </div>
+      </div>
+      <div className="info-card">
+        <AiOutlineMail id="email-icon" />
+        <div>
+          <h3>E-mail</h3>
+          <p>guilhermehenrique
+            .profissional@gmail.com</p>
+        </div>
+      </div>
+      <div className="info-card">
+        <AiFillEnvironment id="pin-icon" />
+        <div>
+          <h3>Localização</h3>
+          <p>Cabo de Santo Agostinho / PE</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default InformationContainer;
